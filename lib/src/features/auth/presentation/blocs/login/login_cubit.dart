98138ff236f83426aad91be4f5e143130e878cba
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-11 21:34:12 +0200

    login cubit

--- lib/src/features/auth/presentation/blocs/login/login_cubit.dart
+++ lib/src/features/auth/presentation/blocs/login/login_cubit.dart
@@ -1,8 +1,77 @@
-import 'package:bloc/bloc.dart';
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../domain/entities/logged_in_user.dart';
+import '../../../domain/usecases/login_user.dart';
+import 'package:formz/formz.dart';
+
+import '../../../../../shared/domain/entities/user.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  //private! uso lista iniziale nel costruttore
+  final LoginUser _loginUserUsecase;
+  LoginCubit({required LoginUser loginUser})
+      : _loginUserUsecase = loginUser,
+        super(LoginState.initial());
+
+  //METODI
+  //il value arriva dal form
+  void usernameChanged(String value) {
+    final username = Username.dirty(value); //creo il value object
+    final status = Formz.validate([
+      username,
+      state.password,
+    ]);
+
+    emit(state.copyWith(
+        username: username,
+        status: status
+            ? FormzSubmissionStatus.initial
+            : FormzSubmissionStatus
+                .failure)); //ho emesso lo stato con nuovo username e lo status pari a se il nuovo
+    // username e la password rimnasta come prima sono validi
+  }
+
+  void passwordChanged(String value) {
+    final password = Password.dirty(value);
+    final status = Formz.validate([
+      state.username,
+      password,
+    ]);
+    emit(state.copyWith(
+        password: password,
+        status: status
+            ? FormzSubmissionStatus.initial
+            : FormzSubmissionStatus.failure));
+  }
+
+  FutureOr<void> loginWithCredentials() async {
+    emit(state.copyWith(
+        status: FormzSubmissionStatus
+            .inProgress)); //valido con la logica di business
+    if (!state.status.isSuccess) {
+      return; //esco e non faccio nulla se non è success
+    }
+
+    emit(state.copyWith(
+        status: FormzSubmissionStatus
+            .inProgress)); //validato: inizio il login verso la mia API
+    //DO NOT CLICK AGAIN THE BUTTON!
+    try {
+      await _loginUserUsecase(LoginUserParams(
+        username: state.username,
+        password: state.password,
+      ));
+      //ho risposta (non eccezione) da API (da backend)
+      emit(state.copyWith(
+          status: FormzSubmissionStatus.success)); //emetto stato per dire OK!
+    } catch (err) {
+      emit(state.copyWith(
+          status:
+              FormzSubmissionStatus.failure)); //emesso stato per dire ERROR!
+    }
+  }
 }
--- lib/src/features/auth/presentation/blocs/login/login_state.dart
+++ lib/src/features/auth/presentation/blocs/login/login_state.dart
@@ -1,10 +1,46 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'login_cubit.dart';
 
-abstract class LoginState extends Equatable {
-  const LoginState();
+class LoginState extends Equatable {
+  //input da passare al cubit:
+  final Username username;
+  final Password password;
+  //lo stato del form
+  final FormzSubmissionStatus status;
+  //se c'è un errore
+  final String? errorText;
+
+  const LoginState({
+    this.username = const Username.pure(),
+    this.password = const Password.pure(), //sono empty di default
+    this.status = FormzSubmissionStatus.initial, //non ancora validato
+    this.errorText,
+  });
+
+  factory LoginState.initial() => const LoginState();
 
   @override
-  List<Object> get props => [];
-}
+  List<Object?> get props => [
+        username,
+        password,
+        status,
+        errorText,
+      ];
 
-class LoginInitial extends LoginState {}
+  @override
+  bool? get stringify => true;
+
+  LoginState copyWith({
+    Username? username,
+    Password? password,
+    FormzSubmissionStatus? status,
+    String? errorText,
+  }) {
+    return LoginState(
+      username: username ?? this.username,
+      password: password ?? this.password,
+      status: status ?? this.status,
+      errorText: errorText ?? this.errorText,
+    );
+  }
+}
