34e80653be95175fc984d97b4ad4a88b28663f69
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-12 15:49:38 +0200

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_social_clean/src/config/app_router.dart';
-import 'package:flutter_social_clean/src/config/app_theme.dart';
-import 'package:flutter_social_clean/src/features/auth/data/datasources/mock_auth_datasource.dart';
-import 'package:flutter_social_clean/src/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:loggy/loggy.dart';
+import 'src/logs/bloc_observer.dart';
+import 'src/services/service_locator.dart' as di; //dependecy injection
 
 import 'src/my_app.dart';
 
 void main() {
+  //si assicura che tutto sia inizializzato a livello native
+  WidgetsFlutterBinding.ensureInitialized();
+  //iniziallizza il logger con la stampa a video colorata
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(),
+  );
+  di.init(); //init del service locator
+  //observer di tutti i bloc
+  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
