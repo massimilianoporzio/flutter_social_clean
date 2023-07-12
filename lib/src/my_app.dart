06c9e23597ba2028842242a6b748736de5a80742
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-12 17:07:31 +0200

    service locator su Auth Bloc e Login Cubit

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -4,16 +4,20 @@ import 'package:flutter_social_clean/src/config/app_router.dart';
 import 'package:flutter_social_clean/src/config/app_theme.dart';
 import 'package:flutter_social_clean/src/features/auth/data/datasources/mock_auth_datasource.dart';
 import 'package:flutter_social_clean/src/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:flutter_social_clean/src/features/auth/presentation/blocs/login/login_cubit.dart';
+
+import 'features/auth/presentation/blocs/auth/auth_bloc.dart';
+import 'services/service_locator.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  //TODO: fare con GETIT
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     //DO' accesso A TUTTA LA APP AL MIO REPOSITORY (TODO: meglio con injection)
     return MultiRepositoryProvider(
+      //TODO decidere se tenere o no
       providers: [
         RepositoryProvider(
           create: (context) => AuthRepositoryImpl(
@@ -23,16 +27,13 @@ class MyApp extends StatelessWidget {
       ],
       //poi do' accesso a TUTTA la APP ai miei bloc
       child: MultiBlocProvider(
-        providers: const [
-          // BlocProvider(
-          //   create: (context) => AuthBloc(
-          //       logoutUser: logoutUser,
-          //       getAuthStatus: getAuthStatus,
-          //       getLoggedInUser: getLoggedInUser),
-          // ),
-          // BlocProvider(
-          //   create: (context) => LoginCubit(loginUser: loginUser),
-          // ),
+        providers: [
+          BlocProvider(
+            create: (context) => sl<AuthBloc>(),
+          ),
+          BlocProvider(
+            create: (context) => sl<LoginCubit>(),
+          ),
         ],
         child: MaterialApp.router(
           debugShowCheckedModeBanner: false,
--- lib/src/services/service_locator.dart
+++ lib/src/services/service_locator.dart
@@ -1,9 +1,43 @@
+import 'package:flutter_social_clean/src/features/auth/data/datasources/mock_auth_datasource.dart';
+import 'package:flutter_social_clean/src/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:flutter_social_clean/src/features/auth/domain/repositories/auth_repository.dart';
+import 'package:flutter_social_clean/src/features/auth/domain/usecases/get_auth_status.dart';
+import 'package:flutter_social_clean/src/features/auth/domain/usecases/get_logged_in_user.dart';
+import 'package:flutter_social_clean/src/features/auth/domain/usecases/login_user.dart';
+import 'package:flutter_social_clean/src/features/auth/domain/usecases/logout_user.dart';
+import 'package:flutter_social_clean/src/features/auth/domain/usecases/signup_user.dart';
+import 'package:flutter_social_clean/src/features/auth/presentation/blocs/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  //*DATASOURCES:
+  //AUTH
+  sl.registerLazySingleton<MockAuthDatasource>(() => MockAuthDatasourceImpl());
+  //*REPOSITORIES:
+  //AUTH:
+  sl.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(authDatasource: sl<MockAuthDatasource>()));
+  //*USECASES
+  //AUTH:
+  sl.registerLazySingleton<GetAuthStatus>(
+      () => GetAuthStatus(authRepository: sl<AuthRepository>()));
+  sl.registerLazySingleton<GetLoggedInUser>(
+      () => GetLoggedInUser(authRepository: sl<AuthRepository>()));
+  sl.registerLazySingleton<LoginUser>(
+      () => LoginUser(authRepository: sl<AuthRepository>()));
+  sl.registerLazySingleton<LogoutUser>(
+      () => LogoutUser(authRepository: sl<AuthRepository>()));
+  sl.registerLazySingleton<SignupUser>(
+      () => SignupUser(authRepository: sl<AuthRepository>()));
+
   //*BLOCS / CUBITS
 
   //Auth
+  sl.registerFactory<AuthBloc>(() => AuthBloc(
+        logoutUser: sl<LogoutUser>(),
+        getAuthStatus: sl<GetAuthStatus>(),
+        getLoggedInUser: sl<GetLoggedInUser>(),
+      ));
 }
